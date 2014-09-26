7f1c38f02efcc6fe07ccf726190b5583a57a65ad
Josh Sephton <josh.sephton@gmail.com>
2014-09-26 16:48:24 +0100

    Updated the_post() method to jump in and replace $post with a new model

--- classes/query.php
+++ classes/query.php
@@ -13,6 +13,27 @@ class SQ_Query {
 	}
 
 
+	public function the_post(){
+
+		global $post;
+		$this->in_the_loop = true;
+
+		if($this->current_post == -1){
+			do_action_ref_array('loop_start', array(&$this));
+		}
+
+		$post = $this->next_post();
+
+		$model = 'SQ_Model_'.$post->post_type;
+		if(class_exists($model)){
+			$post = new $model($post);
+		}
+
+		setup_postdata($post);
+
+	}
+
+
 	public function __get($property){
 
 		if(property_exists($this->wp_query, $property)){
