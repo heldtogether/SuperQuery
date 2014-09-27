5db7a718d7bd1f3c2b92d888c1dfe50a39271dfc
Josh Sephton <josh.sephton@gmail.com>
2014-09-27 10:49:01 +0100

    Updated query docblock

--- classes/query.php
+++ classes/query.php
@@ -1,18 +1,52 @@
 <?php
 
+/**
+ * Query
+ * 
+ * Constructed with WP_Query and 
+ * passes through most method calls 
+ * and property requests to the
+ * original. Overloads `the_post()`
+ * method to check if a model class
+ * exists for the post type.
+ *
+ * @package SuperQuery
+ * @author Josh Sephton
+ **/
 class SQ_Query {
 
 
+	/**
+	 * The query on which this query
+	 * is based.
+	 *
+	 * @var WP_Query
+	 */
 	protected $wp_query;
 
 
-	public function __construct($wp_query){
-
+	/**
+	 * Construct
+	 *
+	 * @param WP_Query the original post
+	 * @return void
+	 **/
+	public function __construct(WP_Query $wp_query){
 		$this->wp_query = $wp_query;
-
 	}
 
 
+	/**
+	 * Get the current post.
+	 *
+	 * It first checks if a model
+	 * class exists for the post type.
+	 * If available, it creates the
+	 * post as the custom class, else
+	 * defaults back to WP_Post.
+	 *
+	 * @return void
+	 **/
 	public function the_post(){
 
 		global $post;
@@ -34,22 +68,28 @@ class SQ_Query {
 	}
 
 
+	/**
+	 * Get magic method
+	 *
+	 * @return mixed
+	 **/
 	public function __get($property){
-
 		if(property_exists($this->wp_query, $property)){
 			return $this->wp_query->$property;
 		}
-
 		throw new InvalidArgumentException;
 	}
 
 
+	/**
+	 * Call magic method
+	 *
+	 * @return mixed
+	 **/
 	public function __call($method, $arguments){
-
 		if(method_exists($this->wp_query, $method)){
 			return $this->wp_query->$method();
 		}
-
 		throw new BadMethodCallException;
 	}
 
